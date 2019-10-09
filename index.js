d3de18dae0f946a07ec71928a995455bbff27896
Keane Le <keanele12345@gmail.com>
2019-10-08 23:15:08 -0700

    express, get / route added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// some api
+const express = require('express');
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Canary deployment test API version 1!",
+        version: "1.0"
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Canary deployment test API started!");
+});
