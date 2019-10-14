2780b0e6797729d669c433034b6d1bea17da83f4
Keane Le <keanele12345@gmail.com>
2019-10-14 10:50:31 -0700

    to green

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const app = express();
 
 
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to Canary deployment test API version 1!",
-        version: "1.0",
-        environment: "blue"
-    }); // BLUE
-});
-
 // app.get("/", (req, res) => {
 //     res.json({
-//         message: "Welcome to Canary deployment test API version 2!",
-//         version: "2.0",
-//         environment: "green"
-//     }); // GREEN
+//         message: "Welcome to Canary deployment test API version 1!",
+//         version: "1.0",
+//         environment: "blue"
+//     }); // BLUE
 // });
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Canary deployment test API version 2!",
+        version: "2.0",
+        environment: "green"
+    }); // GREEN
+});
 
 app.listen(80, () => {
     console.log("Canary deployment test API started!");
